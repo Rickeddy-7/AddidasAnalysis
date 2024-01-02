391531c87cf53f250e8da8ca577e6c2c2969c07c
rickeddy <rickeddy3010@gmail.com>
2024-01-02 07:09:23 +0200

    added if main check

--- DataDownload.py
+++ DataDownload.py
@@ -4,19 +4,27 @@ import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 
-# We first create an api instance and verify our api key with the authenticate method call
-api = KaggleApi()
-api.authenticate()
+def run_main():
+    '''download the dataset using the kaggle api'''
 
-# get the data and store it in the project directory
-api.dataset_download_file(
-    dataset='thedevastator/adidas-fashion-retail-products-dataset-9300-prod',
-    file_name='adidas_usa.csv'
-)
+    # We first create an api instance and verify our api key with the authenticate method call
+    api = KaggleApi()
+    api.authenticate()
 
-# unzip and delete the file
-zip_file = os.getcwd() + '/adidas_usa.csv.zip'
-with zipfile.ZipFile(zip_file, 'r') as file:
-    file.extractall()
+    # get the data and store it in the project directory
+    api.dataset_download_file(
+        dataset='thedevastator/adidas-fashion-retail-products-dataset-9300-prod',
+        file_name='adidas_usa.csv'
+    )
 
-os.remove(zip_file)
+    # unzip and delete the file
+    zip_file = os.getcwd() + '/adidas_usa.csv.zip'
+    with zipfile.ZipFile(zip_file, 'r') as file:
+        file.extractall()
+
+    os.remove(zip_file)
+
+
+
+if __name__ == "__main__":
+    run_main()
