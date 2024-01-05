2ebf9323f9a8502bb7cae50299488a88b3d2dd57
rickeddy <rickeddy3010@gmail.com>
2024-01-05 12:59:18 +0200

    price formatting function done

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,14 +1,42 @@
 
 import pandas as pd
+import numpy as np
 
 
-# def clean_data():
-#     '''preprocess the data for analysis'''
+def clean_data():
+    '''preprocess the data for analysis'''
+
+    df = pd.read_csv(r'C:\Users\ricke\AddidasAnalysis\adidas_usa.csv')
+    # call other functions...
+    df.to_csv()
 
 
-df = pd.read_csv(r'C:\Users\ricke\AddidasAnalysis\adidas_usa.csv')
+def format_price(df: pd.DataFrame):
+    '''remove the dollar sign from the price and get the average price to impute missing values'''
 
-query = df['original_price'].notnull().replace(df['original_price'][:1], '')
-query2 = df['original_price']
+    df['original_price'] = pd.to_numeric(df['original_price'].str.replace('$', ''))
+    mean_price = int(np.nanmean(df['original_price']))
+    df['original_price'] = df['original_price'].fillna(str(mean_price)).astype(int)
 
-print(query2)
+
+def remove_columns(df: pd.DataFrame):
+    '''delete unwanted fields'''
+
+    cols = ['url', 'sku', 'currency', 'availability', 'source', 'source_website', 'description', 'brand', 'images', 'country', 'language']
+    df.drop(columns=cols)
+
+
+def rename_columns(df: pd.DataFrame):
+    '''rename a field to the specified new name'''
+    df.rename(columns=['breadcumbs', 'crawled_at'])
+
+
+# rename 'breadcrumbs' to 'gender' and remove the category
+
+# rename 'crawled_at' to 'date' and remove the time
+
+# create a new fields for the currency in ZAR
+
+# create a new field for the revenue in ZAR
+
+# create a new field for the discounted amount(difference of selling price and original price) in ZAR
