a849e3a48873af65e1580830ee89ce981ca47813
rickeddy <rickeddy3010@gmail.com>
2024-01-07 10:47:15 +0200

    removed return statements

--- DataCleaning.py
+++ DataCleaning.py
@@ -19,35 +19,34 @@ def format_price(df: pd.DataFrame):
     mean_price = int(np.nanmean(df['original_price']))
     df['original_price'] = df['original_price'].fillna(str(mean_price)).astype(int)
 
-    return df
+
 
 
 def remove_columns(df: pd.DataFrame):
     '''delete unwanted fields'''
 
     cols = ['url', 'sku', 'currency', 'availability', 'source', 'source_website', 'description', 'brand', 'images', 'country', 'language']
-    return df.drop(columns=cols)
+    df.drop(columns=cols, inplace=True)
 
 
 def format_date(df: pd.DataFrame):
     '''rename the 'crawled_at' to 'date' and remove the time'''
 
-    return df
+
     
 
 def format_gender(df: pd.DataFrame):
     '''rename 'breadcrumbs' to 'gender' and remove the category'''
 
-    return df
+
 
 
 def add_features(df: pd.DataFrame):
     '''create new fields for the prices in ZAR'''
 
-    # create a new fields for the currency in ZAR
+    # create a new field for the currency in ZAR
 
     # create a new field for the revenue in ZAR
 
     # create a new field for the discounted amount(difference of selling price and original price) in ZAR
 
-    return df
