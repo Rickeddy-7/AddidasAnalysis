198561ebb4a92ee639dbbab9315dd6fa82f73faa
rickeddy <rickeddy3010@gmail.com>
2024-01-13 11:05:16 +0200

    added exchange rate code and new features

--- DataCleaning.py
+++ DataCleaning.py
@@ -4,10 +4,10 @@ import numpy as np
 import requests as rq
 
 
-def clean_data():
+def clean_data(api_key: str):
     '''preprocess the data for analysis'''
 
-    df = pd.read_csv(r'C:\Users\ricke\AddidasAnalysis\adidas_usa.csv')
+    df = pd.read_csv('adidas_usa.csv')
     
     # remove the dollar sign from the price and get the average price to impute missing values
     df['original_price'] = pd.to_numeric(df['original_price'].str.replace('$', ''))
@@ -28,22 +28,26 @@ def clean_data():
     df['sub_category'] = df['sub_category'].str.split('/').str[0]
 
     # create a new field for the currency in ZAR
+    rate: float = get_exchange_rate(api_key)
+    df['original_price_zar'] = list(map(lambda x: round(x*rate, 2), df['original_price']))
 
     # create a new field for the revenue in ZAR
+    df['selling_price_zar'] = list(map(lambda x: round(x*rate, 2), df['selling_price']))
+    
 
     # create a new field for the discounted amount(difference of selling price and original price) in ZAR
 
     # then write all the changes into a new excel file
-    # print("Successfully cleaned data.\n")
-    # df.to_csv('adidas_usa_clean.csv', index=False)
+    print("successfully cleaned data.\n".upper())
+    df.to_csv('adidas_usa_clean.csv', index=False)
 
 
-def convert_currency():
-    '''converts all amounts into South African Rands using the Exchange Rates API'''
+def get_exchange_rate(api_key: str):
+    '''returns the current exchange rate for usd/zar as a float'''
 
-    api_key = 'a1c6d810970de6cc81ac6273'
     response = rq.get(f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD")
-
-    rate = response.json()['conversion_rates']['ZAR']
+    rate: float = response.json()['conversion_rates']['ZAR']
     
+    return rate
 
+# clean_data('a1c6d810970de6cc81ac6273')
