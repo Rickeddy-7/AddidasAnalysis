8f4d4f63d73392a3eaed0c325a922a4fc90457c8
rickeddy <rickeddy3010@gmail.com>
2024-01-13 11:08:36 +0200

    added api key parameter

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 import DataCleaning as dc
 import DataDownload as dd
+import os
 
 
 def run_main():
     '''runs the scripts that download and clean the data for analysis'''
 
     dd.download_data()
-    dc.clean_data()
+    # exch_rate_api_key = os.getenv('api_key')
+    exch_rate_api_key = 'a1c6d810970de6cc81ac6273'
+    dc.clean_data(exch_rate_api_key)
 
 
 if __name__ == "__main__":
